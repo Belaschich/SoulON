aaf9648aacd12fab8a9b3c90ceae0579eec6bdd2
Izabela Schich <izabela.schick@gmail.com>
2022-02-21 14:08:55 -0300

    Create aula1.py

---
+++ Python1/aula1.py
@@ -0,0 +1,12 @@
+'''-Crie quatro variáveis, sendo elas com nomes de animais, quantidade de animais, peso dos animais e se eles são vacinados ou não.
+-Comente seu código usando comentários longos e curtos.'''
+
+nome_animal1 = "Hipopótamo"
+nome_animal2 = "Papagaio"
+nome_animal3 = "Golfinho"
+
+qtd_animais = "3"
+
+peso_animal1 = "40.000kg"
+peso_animal2 = "200g"
+peso_animal3 = "10.000g"
