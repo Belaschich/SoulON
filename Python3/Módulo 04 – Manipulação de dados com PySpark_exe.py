65e28e24859ddcf916d8907c254cd33493f83c80
Izabela Schich <izabela.schick@gmail.com>
2022-03-07 15:35:55 -0300

    .

--- Python3/Módulo 04 – Manipulação de dados com PySpark_exe.py
+++ Python3/Módulo 04 – Manipulação de dados com PySpark_exe.py
@@ -1,15 +1,61 @@
-import findspark
-findspark.init()
+from pyspark.sql.session import SparkSession
 
-from pyspark.sql import SparkSession
+from pyspark.sql.types import (ArrayType, BooleanType, FloatType, IntegerType, StringType, StructField, StructType, TimestampType, DoubleType)
 
-spark = SparkSession.builder \
-    .master("local") \
-    .appName("Aula Introdutória PySpark") \
-    .config("spark.executor.memory", "1gb") \
+import pyspark.sql.functions as F
+
+spark = SparkSession.builder.appName('fisrtSession')\
+    .config('spark.master', 'local[4]')\
+    .config('spark.executor.memory', '1gb')\
+    .config('spark.shuffle.partitions', 1)\
     .getOrCreate()
 
-sc = spark.sparkContext
+schema = StructType([
+      StructField("ID",IntegerType()),
+      StructField("Idade",IntegerType()),
+      StructField("Trabalho",StringType()),
+      StructField("ZipCode",IntegerType()),
+      StructField("Educacao",StringType()),
+      StructField("Numero_Educacao",IntegerType()),
+      StructField("Status_relacionamento",StringType()),
+      StructField("Profissão",StringType()),
+      StructField("Relacionamento",StringType()),
+      StructField("Raca",StringType()),
+      StructField("Sexo",StringType()),
+      StructField("hr_semana",IntegerType()),
+      StructField("País",StringType()),
+      StructField("salario",DoubleType())
+  ])
+path = (r"C:\Users\izabe\Documents\GitHub\SoulON\Python3\adult_data.csv")
 
-rdd = sc.textFile("C:\\Users\\izabe\\Documents\\GitHub\\SoulON\\Python3\\Salary_Data.csv")
-print("Concluído com sucesso!")
+df = spark.read.format('csv')\
+    .schema(schema)\
+    .load(path)
+
+df.printSchema()
+
+df.show(5)
+
+def converterColuna(dataframe, nomes, novoTipo):
+    for nome in nomes:
+        dataframe = dataframe.withColumn(nome, dataframe[nome].cast(novoTipo))
+    return dataframe
+
+colunas = ['Idade']
+
+df = converterColuna(df, colunas, FloatType())
+
+df.show()
+
+df2 = df.select('Idade', 'Educacao')
+
+df2.show(5)
+
+df3 = df.sort(F.desc('Educacao'))
+
+df3.show()
+
+df.describe('salario').show()
+
+
+
